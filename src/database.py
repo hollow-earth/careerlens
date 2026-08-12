879d4054d1f1ec5f86f281b02c758c0031de13d0
hollow-earth <vanctardi@protonmail.com>
2026-08-12 01:48:04 -0400

    Added staging table to db to record jobs before processing them through the LLM

--- src/database.py
+++ src/database.py
@@ -11,7 +11,7 @@ def init_tables(conn: sqlite3.Connection) -> None:
     print(schema_version)
     
     if schema_version == 0 or schema_version == 1:
-        table_creation_query = """
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS jobs (
                 id INTEGER PRIMARY KEY,
                 company TEXT NOT NULL,
@@ -29,11 +29,21 @@ def init_tables(conn: sqlite3.Connection) -> None:
                 short_score TEXT,
                 reasoning TEXT
             )
-            """
+            """)
+        conn.commit()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS staging (
+                id INTEGER PRIMARY KEY,
+                company TEXT NOT NULL,
+                title TEXT NOT NULL,
+                source TEXT NOT NULL,
+                url TEXT NOT NULL UNIQUE,
+            )
+            """)
+        conn.commit()
     else:
         raise Exception("Version not found")
-    cursor.execute(table_creation_query)
-    conn.commit()
 
 def close(conn: sqlite3.Connection) -> None:
     conn.close()
