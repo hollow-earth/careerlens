23108fcc2a415e4e8e28c6d1e11d9c6496a83c6d
hollow-earth <vanctardi@protonmail.com>
2026-08-15 18:02:54 -0400

    Formatting and comments

--- src/scrapers/linkedin.py
+++ src/scrapers/linkedin.py
@@ -2,10 +2,8 @@ from re import search
 from playwright.sync_api import sync_playwright
 from time import sleep
 from random import uniform
-from urllib.parse import urlparse, urlunparse
 from sqlite3 import Connection
-from database import write_job_to_ingest, write_job_to_staging
-from joblisting import JobListing
+from database import write_job_to_ingest
 import scrapers.scraper_utilities as scraper_utilities
 
 def linkedin_scrape_urls(conn: Connection) -> None:
@@ -55,18 +53,19 @@ def linkedin_scrape_urls(conn: Connection) -> None:
                 dismiss_button.click()
             current_job = job_cards.nth(i)
             current_job.click()
-
+            
+            # Scrape URL first, remove the subdomain and text after jobs/view/{numerical_id}
             scraped_url = current_job.locator(".base-card__full-link").get_attribute("href")
             assert scraped_url is not None, "Expected href attribute to be a string, but got None"
             match = search(r"jobs/view/(?:.+\-)?(\d+)/?", scraped_url)
             assert match is not None, f"Regex failed to extract a Job ID from the URL: {scraped_url}"
             job_id = match.group(1).strip()
             scraped_url = f"https://www.linkedin.com/jobs/view/{job_id}"
-
+            
+            # If it already exists in ingest, staging, or jobs, skip adding to ingest
             if not scraper_utilities.job_exists_in_pipeline(conn, "linkedin", job_id):
                 write_job_to_ingest(conn, "linkedin", job_id, scraped_url)
-
-            i += 1
+            
             # Load more jobs if needed, then update the current list of jobs, then grab the next
             show_more_jobs_button = page.locator(".infinite-scroller__show-more-button")
             if show_more_jobs_button.count() and show_more_jobs_button.is_visible():
@@ -76,6 +75,7 @@ def linkedin_scrape_urls(conn: Connection) -> None:
                 sleep(uniform(1.0, 3.0))
             job_cards = page.locator("ul.jobs-search__results-list > li")
             count = job_cards.count()
+            i += 1
             sleep(uniform(1.0, 3.0))
         browser.close()
 
