82566d2acdd13aa1006cce5478b833ed595702b5
hollow-earth <vanctardi@protonmail.com>
2026-08-21 15:32:39 -0400

    Time for a temporary mess

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 from typing import Any
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Playwright, Browser, sync_playwright
 from tomllib import load
 
 import database
@@ -17,6 +17,35 @@ def load_config() -> tuple[dict[str, Any], JobFilters]:
         config = load(f)
     return config, JobFilters(config)
 
+def process_job(conn, config, row):
+    id, job, created_at = row
+    min_score = config["llm"]["minimum_score"]
+    print(f"Processing job: {job.title}, at {job.company}")
+    job_to_write = llm.use_llm(config, job)
+    with conn:
+        if job_to_write.score >=min_score:
+            database.write_job_to_jobs(conn, job_to_write, created_at)
+        else:
+            database.write_job_to_discarded(
+                conn, 
+                job_to_write, 
+                f"Score {job_to_write.score} below the minimum threshold of {min_score}", 
+                created_at,
+                datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            )
+        database.delete_from_staging(conn, id)
+
+def process_jobs_with_llm(conn, config) -> None:
+    while True:
+        start_time = time.perf_counter()
+        row = database.get_next_staging(conn)
+        if row is None:
+            break
+        process_job(conn, config, row)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Processing took {execution_time:.6f}s.\n")
+
 def pipeline():
     config, filters = load_config()
     conn = database.connect()
@@ -26,33 +55,9 @@ def pipeline():
         with sync_playwright() as p:
             browser = p.firefox.launch(headless=False) # TODO: switch to True when tests are over
             linkedin_scraper(conn, browser, config, filters)
-            browser.close()
         # TODO: dedup time
-        
-        while True:
-            start_time = time.perf_counter()
-            row = database.get_next_staging(conn)
-            if row is None:
-                break
-            
-            id, job, created_at = row
-            print(f"Processing job: {job.title}, at {job.company}")
-            job_to_write = llm.use_llm(config, job)
-            with conn:
-                if job_to_write.score >= config["llm"]["minimum_score"]:
-                    database.write_job_to_jobs(conn, job_to_write, created_at)
-                else:
-                    database.write_job_to_discarded(
-                        conn, 
-                        job_to_write, 
-                        f"Score {job_to_write.score} below the minimum threshold of {config["llm"]["minimum_score"]}", 
-                        created_at,
-                        datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    )
-                database.delete_from_staging(conn, id)
-            end_time = time.perf_counter()
-            execution_time = end_time - start_time
-            print(f"Processing took {execution_time:.6f}s.\n")
+
+        process_jobs_with_llm(conn, config)
             
     finally:
         try:
