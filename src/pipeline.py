e4e77a62cbf3bee8b06e7228587491be92347ff8
hollow-earth <vanctardi@protonmail.com>
2026-08-21 15:41:40 -0400

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,13 +3,13 @@ import time
 from datetime import datetime
 from typing import Any
 
-from playwright.sync_api import Playwright, Browser, sync_playwright
+from playwright.sync_api import sync_playwright
 from tomllib import load
 
 import database
 import llm
 from scrapers.linkedin import linkedin_scraper
-from scrapers.scraper_utilities import JobFilters
+from scrapers.scraper_utilities import JobData, JobFilters
 
 
 def load_config() -> tuple[dict[str, Any], JobFilters]:
@@ -17,7 +17,7 @@ def load_config() -> tuple[dict[str, Any], JobFilters]:
         config = load(f)
     return config, JobFilters(config)
 
-def process_job(conn, config, row):
+def process_job_with_llm(conn: sqlite3.Connection, config: dict[str, Any], row: tuple[int, JobData, str]) -> None:
     id, job, created_at = row
     min_score = config["llm"]["minimum_score"]
     print(f"Processing job: {job.title}, at {job.company}")
@@ -35,13 +35,15 @@ def process_job(conn, config, row):
             )
         database.delete_from_staging(conn, id)
 
-def process_jobs_with_llm(conn, config) -> None:
+def drain_staging(conn: sqlite3.Connection, config: dict[str, Any]) -> None:
     while True:
         start_time = time.perf_counter()
-        row = database.get_next_staging(conn)
-        if row is None:
+        
+        row_data = database.get_next_staging(conn)
+        if row_data is None:
             break
-        process_job(conn, config, row)
+        process_job_with_llm(conn, config, row_data)
+        
         end_time = time.perf_counter()
         execution_time = end_time - start_time
         print(f"Processing took {execution_time:.6f}s.\n")
@@ -55,9 +57,9 @@ def pipeline():
         with sync_playwright() as p:
             browser = p.firefox.launch(headless=False) # TODO: switch to True when tests are over
             linkedin_scraper(conn, browser, config, filters)
-        # TODO: dedup time
-
-        process_jobs_with_llm(conn, config)
+       
+        # TODO: deduplicate_staging()
+        drain_staging(conn, config)
             
     finally:
         try:
