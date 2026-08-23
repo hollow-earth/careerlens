00c35768186369b6993b405e146958fa59b48965
hollow-earth <vanctardi@protonmail.com>
2026-08-23 19:57:58 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -22,10 +22,9 @@ def helper_populate_ingest_entries(conn: Connection, source: JobSource, number: 
         if id not in s:
             s.add(id)
             i += 1
-            database.write_job_to_ingest(
-                conn,
-                JobEntry(source, f"{id}", f"https://linkedin.com/{id}"),
-            )
+            job = JobEntry(source, f"{id}", f"https://linkedin.com/{id}")
+            database.write_job_to_ingest(conn, job)
+            database.require_fields(job, database.INGEST_REQUIRED)
     return s
 
 
@@ -115,6 +114,14 @@ def test_ingest_get_next(conn: Connection) -> None:
     assert database.get_next_ingest(conn, JobSource.INDEED) is None
 
 
+def test_ingest_exists_in_pipeline(conn: Connection) -> None:
+    job1 = JobEntry(JobSource.LINKEDIN, "123456789", "https://linkedin.com/123456789")
+    job2 = JobEntry(JobSource.INDEED, "a7a3467cad7fdedb", "https://indeed.com/viewjob?jk=a7a3467cad7fdedb")
+    database.write_job_to_ingest(conn, job1)
+    assert database.job_exists_in_pipeline(conn, job1) is True
+    assert database.job_exists_in_pipeline(conn, job2) is False
+
+
 """
 # ========================================= #
 # 
@@ -124,16 +131,31 @@ def test_ingest_get_next(conn: Connection) -> None:
 """
 
 def helper_populate_staging_entries(conn: Connection, source: JobSource, number: int) -> set[int]:
-    ...
+    s: set[int] = set()
+    i = 0
+    while i < number:
+        id = randint(1, 999999)
+        if id not in s:
+            s.add(id)
+            i += 1
+            job = JobEntry(source, f"{id}", f"https://linkedin.com/{id}",
+                f"SomeTitle{id}", f"SomeCompany{id}", f"SomeLocation{id}", f"SomeDescription{id}")
+            database.write_job_to_staging(conn, job)
+            database.require_fields(job, database.STAGING_REQUIRED)
+    return s
 
 
 def test_staging_insert(conn: Connection) -> None:
-    ...
+    s = helper_populate_staging_entries(conn, JobSource.LINKEDIN, POPULATE_NUMBER_ENTRIES)
+    res = conn.execute("SELECT * FROM staging").fetchall()
+    assert len(res) == len(s)
 
 
 def test_staging_dedups_on_url(conn: Connection) -> None:
-    job1 = JobEntry(JobSource.LINKEDIN, "123456789", "https://linkedin.com/123456789")
-    job2 = JobEntry(JobSource.LINKEDIN, "555555555", "https://linkedin.com/123456789")
+    job1 = JobEntry(JobSource.LINKEDIN, "123456789", "https://linkedin.com/123456789",
+        "SomeTitle", "SomeCompany", "SomeLocation", "SomeDescription")
+    job2 = JobEntry(JobSource.LINKEDIN, "555555555", "https://linkedin.com/123456789",
+        "SomeTitle", "SomeCompany", "SomeLocation", "SomeDescription")
     database.write_job_to_staging(conn, job1)
     database.write_job_to_staging(conn, job2)
     res = conn.execute("SELECT * FROM staging").fetchall()
@@ -141,20 +163,42 @@ def test_staging_dedups_on_url(conn: Connection) -> None:
 
 
 def test_staging_dedups_on_job_id(conn: Connection) -> None:
-    ...
-
-
-def test_staging_dedups_on_rescrape(conn: Connection) -> None:
-    ...
+    job1 = JobEntry(JobSource.LINKEDIN, "123456789", "https://linkedin.com/123456789",
+        "SomeTitle", "SomeCompany", "SomeLocation", "SomeDescription")
+    job2 = JobEntry(JobSource.LINKEDIN, "123456789", "https://linkedin.com/555555555",
+        "SomeTitle", "SomeCompany", "SomeLocation", "SomeDescription")
+    database.write_job_to_staging(conn, job1)
+    database.write_job_to_staging(conn, job2)
+    res = conn.execute("SELECT * FROM staging").fetchall()
+    assert len(res) == 1
 
 
 def test_staging_delete(conn: Connection) -> None:
-    ...
+    s = helper_populate_staging_entries(conn, JobSource.LINKEDIN, POPULATE_NUMBER_ENTRIES)
+    N = randint(1, POPULATE_NUMBER_ENTRIES - 1)
+    for _ in range(N):
+        id = s.pop()
+        database.delete_from_staging(
+            conn,
+            JobEntry(JobSource.LINKEDIN, f"{id}", f"https://linkedin.com/{id}",
+                f"SomeTitle{id}", f"SomeCompany{id}", f"SomeLocation{id}", f"SomeDescription{id}")
+        )
+    res = conn.execute("SELECT * FROM ingest").fetchall()
+    assert len(res) == len(s)
 
 
 def test_staging_get_next(conn: Connection) -> None:
     ...
 
+
+def test_staging_exists_in_pipeline(conn: Connection) -> None:
+    job1 = JobEntry(JobSource.LINKEDIN, "123456789", "https://linkedin.com/123456789",
+        "SomeTitle", "SomeCompany", "SomeLocation", "SomeDescription")
+    job2 = JobEntry(JobSource.INDEED, "a7a3467cad7fdedb", "https://indeed.com/viewjob?jk=a7a3467cad7fdedb",
+        "SomeTitle", "SomeCompany", "SomeLocation", "SomeDescription")
+    database.write_job_to_staging(conn, job1)
+    assert database.job_exists_in_pipeline(conn, job1) is True
+    assert database.job_exists_in_pipeline(conn, job2) is False
 
 
 """
