c9e2ba55ee95b56ee91ffc4faeff13b72f6558d5
hollow-earth <vanctardi@protonmail.com>
2026-09-08 15:56:27 -0400

    Bugfix part 2

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 from collections.abc import Sequence
 from datetime import datetime, timezone
+from pathlib import Path
 
 from scrapers.scraper_utilities import (
     CompanyEntry,
@@ -23,11 +24,14 @@ def connect() -> sqlite3.Connection:
     ------
     sqlite3.Connection: An open database connection.
     """
+
+    db_path = Path("./data/data.db")
+    db_path.parent.mkdir(parents=True, exist_ok=True)
     
     try:
         conn = sqlite3.connect("./data/data.db")
     except sqlite3.Error as error:
-        raise Exception(f"Error: {error}")
+        raise RuntimeError(f"Could not connect to database: {error}") from error
     else:
         conn.row_factory = sqlite3.Row
         return conn
