ca1a906e0bbfb6a00af54657af81e1d4921bbad8
hollow-earth <vanctardi@protonmail.com>
2026-09-25 19:18:47 -0400

    Getting rid of camoufox because it has issues with multithreading; replaced browser with page

--- src/scrapers/scraper_browsers.py
+++ src/scrapers/scraper_browsers.py
@@ -1,7 +1,6 @@
 from contextlib import contextmanager
 from enum import Enum, auto
 
-from camoufox.sync_api import Camoufox
 from playwright.sync_api import sync_playwright
 
 from scrapers.indeed import indeed_scraper
@@ -9,9 +8,9 @@ from scrapers.linkedin import linkedin_scraper
 
 
 class Browsers(Enum):
-    CAMOUFOX = auto()
     PLAYWRIGHT_FIREFOX = auto()
     PLAYWRIGHT_CHROMIUM = auto()
+    PLAYWRIGHT_FIREFOX_CONTEXT = auto()
 
 class ScraperSources(Enum):
     LINKEDIN = auto()
@@ -20,21 +19,38 @@ class ScraperSources(Enum):
 # ScraperSources enum: function from scrapers.module, requires_browser
 SCRAPERS = {
     ScraperSources.LINKEDIN: (linkedin_scraper, Browsers.PLAYWRIGHT_FIREFOX),
-    ScraperSources.INDEED: (indeed_scraper, Browsers.CAMOUFOX)
+    ScraperSources.INDEED: (indeed_scraper, Browsers.PLAYWRIGHT_FIREFOX_CONTEXT)
 }
 
 @contextmanager
 def browser_context(browser_type: Browsers):
     if browser_type is Browsers.PLAYWRIGHT_FIREFOX:
         with sync_playwright() as p:
-            browser = p.firefox.launch(headless=True)
-            yield browser
+            browser = p.firefox.launch(headless=False)
+            page = browser.new_page()
+            yield page
+            browser.close()
 
     elif browser_type is Browsers.PLAYWRIGHT_CHROMIUM:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
-            yield browser
+            page = browser.new_page()
+            yield page
+            browser.close()
 
-    elif browser_type is Browsers.CAMOUFOX:
-        with Camoufox(headless=True) as browser:
-            yield browser
+    elif browser_type is Browsers.PLAYWRIGHT_FIREFOX_CONTEXT:
+        with sync_playwright() as p:
+            context = p.firefox.launch_persistent_context(
+                user_data_dir="./.playwright-profile",
+                headless=False,
+            )
+            context.add_init_script("""
+            Object.defineProperty(navigator, 'webdriver', {
+                get: () => false
+            });
+            """)
+
+            page = context.pages[0]
+            yield page
+            context.close()
+    
