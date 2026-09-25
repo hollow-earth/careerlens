e4ce7f0cdd1d5464987bf5ed4be449c9f31664b7
hollow-earth <vanctardi@protonmail.com>
2026-09-25 17:23:06 -0400

    Basic indeed job card extraction + storing in ingest table

--- src/scrapers/indeed.py
+++ src/scrapers/indeed.py
@@ -1,16 +1,98 @@
 from collections.abc import Callable
+from datetime import datetime, timezone
 from random import uniform
 from sqlite3 import Connection
 
+from bs4 import BeautifulSoup
 from playwright.sync_api import Browser
 from rich.text import Text
+from typing_extensions import Any
+import database
 
-from scrapers.scraper_utilities import JobFilters
+from scrapers.scraper_utilities import CompanyEntry, CompanyTrustStatus, JobEntry, JobFilters, JobSource, JobStatus, normalize, select_directory
 
 PAGE_DELAY = uniform(3.0, 5.0)
 MAX_RETRIES = 3
 ProgressCallback = Callable[[Text], None]
 
-def indeed_scraper(conn: Connection, config: dict[str, object], filters: JobFilters, callback: ProgressCallback) -> None:
-    callback(Text("Extracting Indeed job contents...", style="#f52bfb"))
-    callback(Text("Finished extracting Indeed job contents.", style="#f52bfb"))
+def indeed_extract_contents(conn: Connection, browser: Browser, filters:JobFilters, config: dict[str, Any], progress_callback: ProgressCallback) -> None:
+    country_code = config["indeed"]["country_code"]
+    dir = select_directory()
+    if not dir:
+        return
+    progress_callback(Text(str(dir)))
+    for file in dir.iterdir():
+        if file.suffix.lower() != ".html":
+            continue
+        try:
+            html = file.read_text(encoding="utf-8")
+            soup = BeautifulSoup(html, "html.parser")
+            job_list = soup.select_one("ul.css-pygyny")
+
+            if job_list is None:
+                continue
+
+            for job_card in job_list.find_all("li", recursive=False):
+                card = job_card.select_one(".cardOutline")
+                if not card or card.get("aria-hidden"):
+                    continue
+
+                card = job_card.select_one(".resultContent")
+                if not card:
+                    continue
+
+                title_element = job_card.select_one("a.jcs-JobTitle")
+                if title_element is None:
+                    progress_callback(Text("Failed to extract job card contents!"))
+                    continue 
+
+                company_element = job_card.find("span", attrs={"data-testid": "company-name"})
+                title = title_element.get_text(strip=True) if title_element else None
+                company = company_element.get_text(strip=True) if company_element else None
+                job_id = str(title_element.get("data-jk"))
+                url = f"https://{country_code}.indeed.com/viewjob?jk={job_id}" if country_code else f"https://indeed.com/viewjob?jk={job_id}"
+                if not job_id or not url:
+                    progress_callback(Text(f"Scraping attempt failed for job_id {job_id}"))
+                    continue
+
+                job = JobEntry(title=title, company=company, job_id=job_id, url=url, source=JobSource.INDEED)
+                if not database.job_exists_in_pipeline(conn, job):
+                    with conn:
+                        database.write_job_to_ingest(conn, job)
+                    progress_callback(Text(f"Found job {job.title} at {job.company} with job_id {job.job_id}"))
+                    if job.title is not None and job.company is not None:
+                        with conn:
+                            database.write_company_to_companies(conn, CompanyEntry(job.company))
+                        candidate_company = database.get_company(conn, job.company)
+                        if candidate_company is not None and candidate_company.trust_status.value == CompanyTrustStatus.BLOCKED.value:
+                            job.discard_reason = "Match in blacklisted_companies"
+                            progress_callback(Text("\t Job discarded due to match in blacklisted_companies"))
+                        elif filters.is_title_blacklisted(job.title):
+                            job.discard_reason = "Match in blacklisted_terms"
+                            progress_callback(Text("\t Job discarded due to match in blacklisted_terms"))
+
+                        if job.discard_reason:
+                            job.created_at = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+                            job.discarded_at = job.created_at
+                            job.status = JobStatus.DISCARDED
+                            with conn:
+                                database.write_job_to_discarded(conn, job)
+                                database.delete_from_ingest(conn, job)
+                else:
+                    progress_callback(Text(f"Job {job.title} at {job.company} with job_id {job.job_id} already in database!"))
+
+        except Exception as error:
+            progress_callback(Text(f"Failed to extract contents of file {file}: {error}"))
+            continue
+
+def indeed_extract_url_contents(conn: Connection, browser: Browser, filters:JobFilters, progress_callback: ProgressCallback) -> None:
+    ...
+
+def indeed_scraper(conn: Connection, config: dict[str, object], filters: JobFilters, browser: Browser, progress_callback: ProgressCallback) -> None:
+    progress_callback(Text("Extracting Indeed job contents...", style="#f52bfb"))
+    indeed_extract_contents(conn, browser, filters, config, progress_callback)
+    progress_callback(Text("Finished extracting Indeed job contents.", style="#f52bfb"))
+
+    #progress_callback(Text("Extracting LinkedIn job contents...", style="#f52bfb"))
+    #indeed_extract_url_contents(conn, browser, filters, progress_callback)
+    #progress_callback(Text("Finished extracting LinkedIn job contents.", style="#f52bfb"))
