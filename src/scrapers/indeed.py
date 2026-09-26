b446a21ee5e7e31aeb07aa3f297198984d45d7d8
hollow-earth <vanctardi@protonmail.com>
2026-09-25 21:15:51 -0400

    Functional indeed scraper yippie

--- src/scrapers/indeed.py
+++ src/scrapers/indeed.py
@@ -2,25 +2,33 @@ from collections.abc import Callable
 from datetime import datetime, timezone
 from random import uniform
 from sqlite3 import Connection
+from time import sleep
 
 from bs4 import BeautifulSoup
-from playwright.sync_api import Browser
+from playwright.sync_api import Page
 from rich.text import Text
 from typing_extensions import Any
+
 import database
+from scrapers.scraper_utilities import (
+    CompanyEntry,
+    CompanyTrustStatus,
+    JobEntry,
+    JobFilters,
+    JobSource,
+    JobStatus,
+    select_directory,
+)
 
-from scrapers.scraper_utilities import CompanyEntry, CompanyTrustStatus, JobEntry, JobFilters, JobSource, JobStatus, normalize, select_directory
-
-PAGE_DELAY = uniform(3.0, 5.0)
+PAGE_DELAY = uniform(5.0, 7.0)
 MAX_RETRIES = 3
-ProgressCallback = Callable[[Text], None]
+ProgressCallback = Callable[[Text | str], None]
 
-def indeed_extract_contents(conn: Connection, browser: Browser, filters:JobFilters, config: dict[str, Any], progress_callback: ProgressCallback) -> None:
+def indeed_extract_contents(conn: Connection, filters:JobFilters, config: dict[str, Any], progress_callback: ProgressCallback) -> None:
     country_code = config["indeed"]["country_code"]
     dir = select_directory()
     if not dir:
         return
-    progress_callback(Text(str(dir)))
     for file in dir.iterdir():
         if file.suffix.lower() != ".html":
             continue
@@ -85,14 +93,53 @@ def indeed_extract_contents(conn: Connection, browser: Browser, filters:JobFilte
             progress_callback(Text(f"Failed to extract contents of file {file}: {error}"))
             continue
 
-def indeed_extract_url_contents(conn: Connection, browser: Browser, filters:JobFilters, progress_callback: ProgressCallback) -> None:
-    ...
+def indeed_extract_url_contents(conn: Connection, page: Page, filters:JobFilters, progress_callback: ProgressCallback) -> None:
+    current_retries = 0
+    while True:
+        job = database.get_next_ingest(conn, JobSource.INDEED)
+        if job is None:
+            break
+        try:
+            _ = page.goto(job.url, wait_until="domcontentloaded")
 
-def indeed_scraper(conn: Connection, config: dict[str, object], filters: JobFilters, browser: Browser, progress_callback: ProgressCallback) -> None:
+            job.title = page.get_by_test_id("vj-job-title").inner_text().strip()
+            metadata = page.get_by_test_id("company-info-metadata")
+            company_link = metadata.locator("a")
+            if company_link.count():
+                job.company = company_link.first.inner_text().strip()
+            else:
+                job.company = metadata.get_by_test_id("vj-company-name").inner_text().strip()
+
+            # This is quite fickle, keeping comments below for future tests
+            job.location = metadata.locator(":scope > div").locator(":scope > div").nth(1).inner_text().strip() 
+            #job.location = metadata.locator("div").last.inner_text().strip()
+            #job.location = company_link.locator("xpath=../following-sibling::div[1]").inner_text()
+
+            description = page.locator(".simple-job-description-html").inner_text().strip()
+            compensation_text = metadata.locator("xpath=following-sibling::div[1]").inner_text().strip()
+            job.description = compensation_text + "\n\n" + description
+            job.status = JobStatus.READY
+            progress_callback(Text(f"Found job {job.title} at {job.company} with job ID {job.job_id}"))
+        except Exception as error:
+            progress_callback(f"Error: {error}")
+        try:
+            with conn:
+                database.write_job_to_staging(conn, job)
+                database.delete_from_ingest(conn, job)
+        except Exception as error:
+            progress_callback(f"Couldn't move row from ingest to staging! Error: {error}")
+            current_retries += 1
+            if current_retries > MAX_RETRIES:
+                break
+            continue
+        sleep(PAGE_DELAY)
+
+
+def indeed_scraper(conn: Connection, config: dict[str, object], filters: JobFilters, page: Page, progress_callback: ProgressCallback) -> None:
     progress_callback(Text("Extracting Indeed job contents...", style="#f52bfb"))
-    indeed_extract_contents(conn, browser, filters, config, progress_callback)
+    indeed_extract_contents(conn, filters, config, progress_callback)
     progress_callback(Text("Finished extracting Indeed job contents.", style="#f52bfb"))
 
-    #progress_callback(Text("Extracting LinkedIn job contents...", style="#f52bfb"))
-    #indeed_extract_url_contents(conn, browser, filters, progress_callback)
-    #progress_callback(Text("Finished extracting LinkedIn job contents.", style="#f52bfb"))
+    progress_callback(Text("Extracting LinkedIn job contents...", style="#f52bfb"))
+    indeed_extract_url_contents(conn, page, filters, progress_callback)
+    progress_callback(Text("Finished extracting LinkedIn job contents.", style="#f52bfb"))
